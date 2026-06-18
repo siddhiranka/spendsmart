b8e21a9cd2bc76a18ebbc900c296de74877b79d4
Siddhi Ranka <siddhiranka@github.com>
2026-06-18 13:37:45 +0530

    Replace bcrypt with bcryptjs to fix Vercel crash

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const { getDB } = require('../config/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { successResponse, errorResponse } = require('../utils/responseHandler');
 
